645ef030cf13e5e72ef69450a922c63fff5bdd44
John M. Wargo <johnwargo@users.noreply.github.com>
2023-03-16 07:38:17 -0400

    coding away.

--- algolia-index-update.js
+++ algolia-index-update.js
@@ -7,18 +7,21 @@ var HighlightType;
 })(HighlightType || (HighlightType = {}));
 import fs from 'fs';
 import path from 'path';
+import indexing from 'algolia-indexing';
 import boxen from 'boxen';
 import chalk from 'chalk';
-const appName = 'Generate Build Info';
-const buildDate = new Date(Date.now());
-const inputFile = path.join(process.cwd(), 'package.json');
-const outputFileName = 'buildinfo.json';
-const newline = "\n";
 const red = HighlightType.Red;
 const yellow = HighlightType.Yellow;
 const green = HighlightType.Green;
-var outputFolder;
-var outputFile;
+const appName = 'Algolia Index Update';
+const defaultFilePath = '_site/algolia.json';
+const newline = "\n";
+const algCredentials = {
+    appId: process.env.ALGOLIA_APP_ID,
+    apiKey: process.env.ALGOLIA_API_KEY,
+    indexName: process.env.ALGOLIA_IDX_NAME
+};
+var inputFilePath;
 function writeConsole(color, highlightText, msg) {
     if (color == HighlightType.Red)
         console.log(newline + chalk.red(`${highlightText}: `) + msg + newline);
@@ -28,28 +31,19 @@ function writeConsole(color, highlightText, msg) {
         console.log(chalk.green(`${highlightText}: `) + msg);
 }
 console.log(boxen(appName, { padding: 1 }));
-writeConsole(HighlightType.Yellow, 'Input file', inputFile);
-try {
-    if (!fs.existsSync(inputFile)) {
-        writeConsole(red, 'Error', 'This is not a nodeJS project, cannot find `package.json` in this folder.');
-        process.exit(1);
-    }
-}
-catch (err) {
-    writeConsole(red, 'Error', err.message);
-    process.exit(1);
-}
+console.dir(algCredentials);
 const pathObj = path.parse(process.argv[0]);
-var tmpStr = pathObj.name == 'node' ? process.argv[2] : process.argv[1];
-if (tmpStr == undefined) {
-    writeConsole(red, 'Error', 'Output folder not specified on command line');
+var commandArg = pathObj.name == 'node' ? process.argv[2] : process.argv[1];
+if (commandArg == undefined) {
+    writeConsole(red, 'Error', 'Module requires at least one argument, `--default` or the input file path.');
     process.exit(1);
 }
-outputFolder = path.join(process.cwd(), tmpStr);
-writeConsole(yellow, 'Output folder', outputFolder);
+let tmpFilePath = commandArg == '--default' ? defaultFilePath : commandArg;
+inputFilePath = path.join(process.cwd(), tmpFilePath);
+writeConsole(yellow, 'Algolia Index', inputFilePath);
 try {
-    if (!fs.existsSync(outputFolder)) {
-        writeConsole(red, 'Error', 'Output folder does not exist, please try again');
+    if (!fs.existsSync(inputFilePath)) {
+        writeConsole(red, 'Error', 'Algolia index file does not exist, please try again');
         process.exit(1);
     }
 }
@@ -57,24 +51,7 @@ catch (err) {
     writeConsole(red, 'Error', err.message);
     process.exit(1);
 }
-outputFile = path.join(outputFolder, outputFileName);
-writeConsole(yellow, 'Output file', outputFile);
-let rawData = fs.readFileSync(inputFile);
-let packageDotJSON = JSON.parse(rawData.toString());
-let buildVersion = packageDotJSON.version;
-writeConsole(green, '\nBuild version', buildVersion);
-writeConsole(green, 'Build date', `${buildDate.toString()} (${buildDate.getTime().toString()} in ms)`);
-const buildInfo = {
-    buildVersion: buildVersion,
-    buildDateMs: buildDate.getTime(),
-    buildDateStr: buildDate.toLocaleString()
-};
-let outputStr = JSON.stringify(buildInfo, null, 2);
-try {
-    fs.writeFileSync(outputFile, outputStr, 'utf8');
-    writeConsole(green, 'Success', 'Output file written successfully\n');
-}
-catch (err) {
-    writeConsole(red, 'Error', 'Unable to write to file');
-    console.dir(err);
-}
+let rawData = fs.readFileSync(inputFilePath);
+let idxData = JSON.parse(rawData.toString());
+console.log(`Processing ${idxData.length} articles.`);
+indexing.verbose();
--- src/algolia-index-update.ts
+++ src/algolia-index-update.ts
@@ -2,13 +2,6 @@
 
 // https://www.raymondcamden.com/2020/06/24/adding-algolia-search-to-eleventy-and-netlify
 
-// =======================================================
-// TODO: Get the input file path from the command line
-// TODO: Use --default to set the default input file path
-// TODO: Make sure the file exists
-// TODO: use Ray's code to update the Algolia index
-// =======================================================
-
 enum HighlightType {
     Red, Yellow, Green
 }
@@ -18,6 +11,8 @@ import fs from 'fs';
 import path from 'path';
 
 // Third-party modules
+//@ts-ignore
+import indexing from 'algolia-indexing';
 import boxen from 'boxen';
 import chalk from 'chalk';
 
@@ -27,11 +22,15 @@ const yellow = HighlightType.Yellow;
 const green = HighlightType.Green;
 
 const appName = 'Algolia Index Update';
-const buildDate = new Date(Date.now());
-const newline="\n";
-const inputFileName = 'algolia.json';
+const defaultFilePath = '_site/algolia.json';
+const newline = "\n";
 
-var inputFolder;
+const algCredentials = {
+    appId: process.env.ALGOLIA_APP_ID,
+    apiKey: process.env.ALGOLIA_API_KEY,
+    indexName: process.env.ALGOLIA_IDX_NAME
+};
+
 var inputFilePath;
 
 function writeConsole(color: HighlightType, highlightText: string, msg: string) {
@@ -42,34 +41,25 @@ function writeConsole(color: HighlightType, highlightText: string, msg: string) 
 
 console.log(boxen(appName, { padding: 1 }));
 
-writeConsole(HighlightType.Yellow, 'Input file', inputFile);
-try {
-    if (!fs.existsSync(inputFile)) {
-        writeConsole(red, 'Error',
-            'This is not a nodeJS project, cannot find `package.json` in this folder.');
-        process.exit(1);
-    }
-} catch (err: any) {
-    writeConsole(red, 'Error', err.message);
-    process.exit(1);
-}
+console.dir(algCredentials);
 
-// Check our command-line argument(s)
-// parse the first argument
+// Check our command-line argument
 const pathObj: any = path.parse(process.argv[0]);
 // is it node? Then we have three arguments, otherwise two
-var tmpStr = pathObj.name == 'node'? process.argv[2]: process.argv[1];
-if (tmpStr == undefined) {
-    writeConsole(red, 'Error', 'Output folder not specified on command line');
+var commandArg = pathObj.name == 'node' ? process.argv[2] : process.argv[1];
+if (commandArg == undefined) {
+    writeConsole(red, 'Error', 'Module requires at least one argument, `--default` or the input file path.');
     process.exit(1);
 }
 
-outputFolder = path.join(process.cwd(), tmpStr);
-writeConsole(yellow, 'Output folder', outputFolder);
+let tmpFilePath = commandArg == '--default' ? defaultFilePath : commandArg;
+inputFilePath = path.join(process.cwd(), tmpFilePath);
 
+writeConsole(yellow, 'Algolia Index', inputFilePath);
 try {
-    if (!fs.existsSync(outputFolder)) {
-        writeConsole(red, 'Error', 'Output folder does not exist, please try again');
+    if (!fs.existsSync(inputFilePath)) {
+        writeConsole(red, 'Error',
+            'Algolia index file does not exist, please try again');
         process.exit(1);
     }
 } catch (err: any) {
@@ -77,33 +67,17 @@ try {
     process.exit(1);
 }
 
-outputFile = path.join(outputFolder, outputFileName);
-writeConsole(yellow, 'Output file', outputFile);
+let rawData = fs.readFileSync(inputFilePath);
+let idxData = JSON.parse(rawData.toString());
 
-let rawData = fs.readFileSync(inputFile);
-let packageDotJSON = JSON.parse(rawData.toString());
+console.log(`Processing index for ${idxData.length} articles.`);
 
-let buildVersion = packageDotJSON.version;
-writeConsole(green, '\nBuild version', buildVersion);
-writeConsole(green, 'Build date', `${buildDate.toString()} (${buildDate.getTime().toString()} in ms)`);
+indexing.verbose();
 
-// javascript output
-// let outputStr = 'export const buildInfo = {\n';
-// outputStr += `  buildVersion: "${buildVersion}",\n`;
-// outputStr += `  buildDate: ${buildDate.getTime()},\n`;
-// outputStr += '}';
-
-const buildInfo = {
-    buildVersion: buildVersion,
-    buildDateMs: buildDate.getTime(),
-    buildDateStr: buildDate.toLocaleString()
-};
-let outputStr = JSON.stringify(buildInfo, null, 2);
-
-try {
-    fs.writeFileSync(outputFile, outputStr, 'utf8');
-    writeConsole(green, 'Success', 'Output file written successfully\n');
-} catch (err: any) {
-    writeConsole(red, 'Error', 'Unable to write to file')
-    console.dir(err);
-}
+// const settings = {};
+// try {
+//     await indexing.fullAtomic(algCredentials, data, settings);
+// } catch (e) {
+//     console.log('error in fullAtomic', e);
+// };
+// console.log('Algolia indexing updated. Hopefully.');
